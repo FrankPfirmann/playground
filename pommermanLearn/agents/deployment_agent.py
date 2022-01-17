839ff0a0e4fd1ffd51c8e8259b575e3c02b15741
tgru <21686590+tgru@users.noreply.github.com>
2022-01-17 02:00:20 +0100

    Replace DeploymentAgent calls with TrainAgent calls

--- pommermanLearn/agents/deployment_agent.py
+++ pommermanLearn/agents/deployment_agent.py
@@ -1,29 +1,15 @@
 import torch
-from pommerman import agents
 from pommerman.runner import DockerAgentRunner
 from agents.train_agent import TrainAgent
 from models import Pommer_Q
 from dqn import DQN
 from util.data import transform_observation_partial
 
-from pommerman.agents.base_agent import BaseAgent
-
-class DeploymentAgent(BaseAgent, DockerAgentRunner):
+class DeploymentAgent(DockerAgentRunner):
     """
-    An agent used for training a pytorch model 
+    An agent for deployment.
     """
-
     def __init__(self):
-        self.policy = None
-        pass
-
-    def init_agent(self, id, game_type):
-        """
-        Initializes the agent and sets the model to train 
-
-        :param policy: The pytorch model to train
-        """
-        super(DeploymentAgent, self).__init__()
         model_dir="./data/models/deployment.pkl"
 
         q = Pommer_Q(11*2-1, transform_observation_partial)
@@ -31,10 +17,17 @@ class DeploymentAgent(BaseAgent, DockerAgentRunner):
 
         algo = DQN(q, q_target, is_train=False, device=torch.device("gpu"))
         algo.q_network.load_state_dict(torch.load(model_dir))
-        self.policy = algo.get_policy()
 
-        #self.agent=TrainAgent(policy=self.policy)
-	
-    def act(self, obs, action_space):
-        act = self.policy(obs)
-        return int(act.detach().numpy()[0])
+        self._agent=TrainAgent(policy=algo.get_policy())
+
+    def init_agent(self, id, game_type):
+        return self._agent.init_agent(id, game_type)
+
+    def act(self, observation, action_space):
+        return self._agent.act(observation, action_space)
+
+    def episode_end(self, reward):
+        return self._agent.episode_end(reward)
+
+    def shutdown(self):
+        return self._agent.shutdown()
