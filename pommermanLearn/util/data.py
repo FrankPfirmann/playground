b49a6176af1198e908e7d440a1aee4696215ee6e
tgru <21686590+tgru@users.noreply.github.com>
2022-02-05 23:30:58 +0100

    Rework transform_observation()
    - Modularize the code for better readablity
    - Fix a bug where the fog was applied wrongly
    - Fix a bug where the board was centralized, despite centralized=False
    - Add unit tests

--- pommermanLearn/util/data.py
+++ pommermanLearn/util/data.py
@@ -1,10 +1,7 @@
 import numpy as np
 from pommerman.constants import Item
 
-from util.analytics import Stopwatch
-
-
-def transform_observation(obs, p_obs=False, centralized=False):
+def transform_observation(obs: dict, p_obs: bool=False, centralized: bool=False):
     """
     Transform a singular observation of the board into a stack of
     binary planes.
@@ -15,86 +12,112 @@ def transform_observation(obs, p_obs=False, centralized=False):
 
     :return: A stack of binary planes
     """
-    features = {}
-    board = obs['board']
-    plane_type=np.float64
-    planes = [                                                  # Index
-        np.isin(board, Item.Passage.value).astype(plane_type),    # 0
-        np.isin(board, Item.Rigid.value).astype(plane_type),      # 1
-        np.isin(board, Item.Wood.value).astype(plane_type),       # 2
-        np.isin(board, Item.Bomb.value).astype(plane_type),       # 3
-        np.isin(board, Item.Flames.value).astype(plane_type),     # 4
-        np.isin(board, Item.ExtraBomb.value).astype(plane_type),  # 5
-        np.isin(board, Item.IncrRange.value).astype(plane_type),  # 6
-        np.isin(board, Item.Kick.value).astype(plane_type),       # 7
-        np.isin(board, Item.Agent0.value).astype(plane_type),     # 8
-        np.isin(board, Item.Agent1.value).astype(plane_type),     # 9
-        np.isin(board, Item.Agent2.value).astype(plane_type),     # 10
-        np.isin(board, Item.Agent3.value).astype(plane_type),     # 11
-        np.array(obs['flame_life']).astype(plane_type),
-        np.array(obs['bomb_life']).astype(plane_type)
-        #np.isin(board, Item.Fog.value).astype(np.uint8)         # 12
-    ]
-    if p_obs:
-        planes = _centralize_planes_partial(planes, obs['position'])
-    elif centralized:
-        planes = _centralize_planes(planes, obs['position'])
+    if p_obs and not centralized:
+        raise ValueError("Invalid value combination (p_obs=True and centralized=False)")
 
-    transformed = np.stack(planes, axis=-1)
+    board = obs['board']
+    view_type=np.float64
+    views = [                                                    # Index
+        np.isin(board, Item.Passage.value).astype(view_type),    # 0
+        np.isin(board, Item.Rigid.value).astype(view_type),      # 1
+        np.isin(board, Item.Wood.value).astype(view_type),       # 2
+        np.isin(board, Item.Bomb.value).astype(view_type),       # 3
+        np.isin(board, Item.Flames.value).astype(view_type),     # 4
+        np.isin(board, Item.ExtraBomb.value).astype(view_type),  # 5
+        np.isin(board, Item.IncrRange.value).astype(view_type),  # 6
+        np.isin(board, Item.Kick.value).astype(view_type),       # 7
+        np.isin(board, Item.Agent0.value).astype(view_type),     # 8
+        np.isin(board, Item.Agent1.value).astype(view_type),     # 9
+        np.isin(board, Item.Agent2.value).astype(view_type),     # 10
+        np.isin(board, Item.Agent3.value).astype(view_type),     # 11
+        np.array(obs['flame_life']).astype(view_type),           # 12
+        np.array(obs['bomb_life']).astype(view_type)             # 13
+    ]
+    if centralized and p_obs:
+        views.append(np.isin(board, Item.Fog.value).astype(np.uint8)) # 14
+    
+    if centralized and not p_obs:
+        views.append(np.zeros(board.shape))
+    
+    transformed = []
+    for view in views:
+        if centralized or p_obs:
+            padding = 1 if view is views[-1] else 0
+            view = centralize_view(view, obs['position'], padding=padding) 
+        if p_obs:
+            view = crop_view(view, view_range=4)
+
+        transformed.append(view)
+
+    transformed = np.stack(transformed, axis=-1)
     transformed = np.moveaxis(transformed, -1, 0)  # move channel dimension to front (pytorch expects this)
-    features['board'] = transformed
     return transformed
 
+def centralize_view(view: np.array, position: np.array, padding: int=0):
+    """
+    Centralize the view around the given position.
 
-def _centralize_planes(planes, pos):
-    b_size = planes[0].shape[0]
-    central_b_size = 2 * b_size - 1
-    centralized_planes = []
-    for p in planes:
-        central = np.zeros((central_b_size, central_b_size))
-        start = ((b_size - 1) - pos[0], (b_size - 1) - pos[1])
-        central[start[0]:start[0] + b_size, start[1]:start[1] + b_size] = p
-        centralized_planes.append(central)
+    :param view: This view will be centered around position. Must have
+        an odd width and height.
+    :param position: This position in the view will be the new center
+        point
+    :param padding: Areas outside the view will be padded with this
+        value
+    """
+    if not view.shape[0]%2 and not view.shape[0]%2:
+        raise ValueError("view width and height must be odd numbers")
 
-    outside_board = np.zeros((central_b_size, central_b_size))
-    outside_board[max((b_size - 1) - pos[0], 0):min(max((b_size - 1) - pos[0], 0) + b_size, central_b_size),
-    max((b_size - 1) - pos[1], 0):min(max((b_size - 1) - pos[1], 0) + b_size, central_b_size)] = np.ones(
-        (b_size, b_size))
-    centralized_planes.append(outside_board)
-    return centralized_planes
+    centralized = np.ones(tuple([2*d-1 for d in view.shape])) * padding
 
+    ax, ay = position # Agent position
+    cx, cy = [int((d-1)/2) for d in centralized.shape] # Center position
+    pw, ph = view.shape # Board width and height
 
-def _centralize_planes_partial(planes, pos):
-    b_size = 5
-    central_b_size = 2 * b_size - 1
-    partial_planes = []
-    board_length = len(planes[0][0])
-    for p in planes:
-        partial = np.zeros((central_b_size, central_b_size))
+    left  = cx   - ax
+    right = left + pw
+    up    = cy   - ay
+    down  = up   + ph
 
-        partial[max((b_size-1) - pos[0], 0):min(board_length - pos[0] + b_size-1, central_b_size),
-        max((b_size-1) - pos[1], 0):min(board_length - pos[1] + b_size-1, central_b_size)] = \
-            p[max(pos[0] - (b_size - 1), 0):min(board_length, pos[0] + (b_size)),
-            max(pos[1] - (b_size - 1), 0):min(board_length, pos[1] + (b_size))]
-        partial_planes.append(partial)
-    outside_board = np.zeros((central_b_size, central_b_size))
-    outside_board[max((b_size-1) - pos[0], 0):min(max((b_size-1) - pos[0], 0) + b_size, central_b_size),
-        max((b_size-1) - pos[1], 0):min(max((b_size-1) - pos[1], 0) + b_size, central_b_size)] = np.ones((b_size, b_size))
-    partial_planes.append(outside_board)
-    return partial_planes
+    # Copy the small plane in the center of the big centralized plane 
+    centralized[left:right, up:down] = view
 
+    return centralized
+    
+def crop_view(view: np.array, view_range: int):
+    """
+    Crops the view rectangularly to the given view_range around the
+    views center point.
+    
+    :param view: The view crop. Width and height must be odd numbers.
+    :param view_range: The number of columns and rows left in each
+        direction after cropping, counting from the center point. Must
+        be greater then zero and not exceed any view bound.
+    """
+    if not view.shape[0]%2 or not view.shape[1]%2:
+        raise ValueError("view width and height must be odd numbers")
+
+    if view_range <= 0:
+        raise ValueError("view_range must be greater then zero")
+
+    if view_range > min([(d-1)/2 for d in view.shape]): # Check if view range exceeds any view bound
+        raise ValueError("view_range must not exceed any view bound")
+
+    width, height = view.shape
+    left  = int( (width-1) / 2 - view_range )
+    right = int( width - left )
+    up    = int( (height-1) / 2 - view_range )
+    down  = int( height - up )
+
+    return view[left:right, up:down]
 
 def transform_observation_centralized(obs):
     return transform_observation(obs, p_obs=False, centralized=True)
 
-
 def transform_observation_partial(obs):
-    return transform_observation(obs, p_obs=True)
-
+    return transform_observation(obs, p_obs=True, centralized=True)
 
 def transform_observation_simple(obs):
     return transform_observation(obs)
-
 
 def calc_dist(agent_ind, nobs, teammate_ind=-1):
     other_inds = [i for i in range(0, len(nobs))]
--- tests/pommermanLearn/util/test_data.py
+++ tests/pommermanLearn/util/test_data.py
@@ -1,6 +1,10 @@
 import pytest
+import os
+import pickle
 import numpy as np
-from util.data import merge_views
+from pommermanLearn.util.data import crop_view, merge_views, centralize_view, transform_observation
+
+RES_DIR = os.path.dirname(os.path.abspath(__file__))
 
 first=np.array([
     [0.0, 0.0, 0.0, 0.0],
@@ -25,7 +29,7 @@ fov=np.array([
 ])
 
 forgetfullness=0.2
-expected=np.array([
+expected_merge=np.array([
     [0.0, 0.0, 0.0, 0.0],
     [0.4, 1.0, 1.0, 0.0],
     [0.0, 0.0, 0.0, 0.0],
@@ -51,7 +55,7 @@ def test_merge_planes_forgetfullness_zero():
 
 def test_merge_planes_forgetfullness_inbetween():
     actual = merge_views(first, second, fov, forgetfullness=forgetfullness)
-    assert np.array_equal(actual, expected)
+    assert np.array_equal(actual, expected_merge)
 
 def test_merge_planes_forgetfullness_one():
     merge_views(first, second, fov, 1.0)
@@ -63,3 +67,95 @@ def test_merge_planes_forgetfullness_negative():
 def test_merge_planes_forgetfullness_over_one():
     with pytest.raises(Exception):
         merge_views(first, second, fov, 1.1)
+    
+# centralize_plane
+position = [1, 2]
+plane_decentralized = np.array([
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+])
+plane_centralized_padding_zeros = np.array([
+    [0, 0, 0, 0, 0],
+    [0, 1, 2, 0, 0],
+    [3, 4, 5, 0, 0],
+    [6, 7, 8, 0, 0],
+    [0, 0, 0, 0, 0]
+])
+plane_centralized_padding_ones = np.array([
+    [1, 1, 1, 1, 1],
+    [0, 1, 2, 1, 1],
+    [3, 4, 5, 1, 1],
+    [6, 7, 8, 1, 1],
+    [1, 1, 1, 1, 1]
+])
+
+def test_centralize_plane_padding_zeros():
+    actual = centralize_view(plane_decentralized, position=position, padding=0)
+    print(actual)
+    print(plane_centralized_padding_zeros)
+    assert np.array_equal(actual, plane_centralized_padding_zeros)
+
+def test_centralize_plane_padding_ones():
+    actual = centralize_view(plane_decentralized, position=position, padding=1)
+    assert np.array_equal(actual, plane_centralized_padding_ones)
+
+# test_crop_view
+view_full       = np.arange(25).reshape(5,5)
+view_range_good = 1
+view_range_bad  = 3
+view_cropped    = np.array([
+    [6,  7,  8],
+    [11, 12, 13],
+    [16, 17, 18]
+])
+
+def test_crop_view_reject_even_width():
+    with pytest.raises(ValueError):
+        crop_view(np.ones((4,5)), view_range_good)
+    
+def test_crop_view_reject_even_height():
+    with pytest.raises(ValueError):
+        crop_view(np.ones((5,4)), view_range_good)
+
+def test_crop_view_reject_even_width_and_height():
+    with pytest.raises(ValueError):
+        crop_view(np.ones((4,4)), view_range_good)
+
+def test_crop_view_reject_view_range_negative():
+    with pytest.raises(ValueError):
+        crop_view(view_full, -1)
+
+def test_crop_view_reject_view_range_zero():
+    with pytest.raises(ValueError):
+        crop_view(view_full, 0)
+
+def test_crop_view_reject_view_range_out_of_bounds():
+    with pytest.raises(ValueError):
+        crop_view(view_full, view_range_bad)
+
+def test_crop_view():
+    actual = crop_view(view_full, view_range_good)
+    assert np.array_equal(actual, view_cropped)
+
+# transform_observation
+obs         = pickle.load(open(os.path.join(RES_DIR, "obs.pkl"), 'rb'))
+expected_ff = np.load(os.path.join(RES_DIR, "expected_ff.npy"), allow_pickle=True)
+expected_ft = np.load(os.path.join(RES_DIR, "expected_ft.npy"), allow_pickle=True)
+expected_tt = np.load(os.path.join(RES_DIR, "expected_tt.npy"), allow_pickle=True)
+
+def test_transform_observation_ff():
+    actual = transform_observation(obs, p_obs=False, centralized=False)
+    assert np.array_equal(actual, expected_ff)
+
+def test_transform_observation_ft():
+    actual = transform_observation(obs, p_obs=False, centralized=True)
+    assert np.array_equal(actual, expected_ft)
+
+def test_transform_observation_tf():
+    with pytest.raises(ValueError):
+        transform_observation(obs, p_obs=True, centralized=False)
+
+def test_transform_observation_tt():
+    actual = transform_observation(obs, p_obs=True, centralized=True)
+    assert np.array_equal(actual, expected_tt)
