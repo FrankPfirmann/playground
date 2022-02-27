fe32000c74a2bd8dc0a71f59eb2bc1bebe2bf058
tgru <21686590+tgru@users.noreply.github.com>
2022-02-27 12:49:42 +0100

    Extract generate_episode method

--- pommermanLearn/data_generator.py
+++ pommermanLearn/data_generator.py
@@ -109,111 +109,16 @@ class DataGeneratorPommerman:
             counts, average steps
         """
 
-        res = np.array([0.0] * 2)
-        act_counts = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-        ties = 0.0
-        avg_rwd = 0.0
-        avg_steps = 0.0
-        fifo = [[] for _ in range(self.agents_n)]
-        skynet_reward_log = [[0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0]]
-        for i_episode in range(episodes):
-            agent_inds, agent_ids, agent_list = self._init_agent_list(agent1, agent2, policy1, policy2
-                                                                      , enemy, transformer, p.set_position)
-            env = pommerman.make(self.env, agent_list)
-            obs = env.reset()
-            done = False
-            ep_rwd = 0.0
-            alive = [True, True]
-            steps_n = 0
-            for i in range(self.agents_n):
-                fifo[i].clear()
+        total_reward = 0.0
+        total_steps = 0.0
+        for _ in range(episodes):
+            reward, steps, res, ties, act_counts, skynet_reward_log = self.generate_episode(agent1, agent2, policy1, policy2, enemy, transformer, max_steps)
+            total_reward += reward
+            total_steps += steps
 
-            while not done:
-                if render and i_episode == 0:
-                    env.render()
-                act = env.act(obs)
-                first_act = [i[0] if type(i) == list else i for i in act]
-                for j in range(self.player_agents_n):
-                    act_counts[j][int(first_act[agent_inds[j]])] += 1
-                nobs, rwd, done, _ = env.step(act)
-                if p.reward_func == "SkynetReward":
-                    skynet_rwds = skynet_reward(obs, act, nobs, fifo, agent_inds, skynet_reward_log)
-                for i in range(self.player_agents_n):
-                    agt: TrainAgent = agent_list[agent_inds[i]]
-                    if p.reward_func == "SkynetReward":
-                        agt_rwd = skynet_rwds[agent_inds[i]]
-                    elif p.reward_func == "BombReward":
-                        agt_rwd = bomb_reward(nobs, act, agent_inds[i])/100
-                    else:
-                        agt_rwd = staying_alive_reward(nobs, agent_ids[i])
-                     # woods close to bomb reward
-                    # if act[agent_inds[i]] == Action.Bomb.value:
-                    #     agent_obs = obs[agent_inds[i]]
-                    #     agt_rwd += woods_close_to_bomb_reward(agent_obs, agent_obs['position'], agent_obs['blast_strength'], agent_ids)
-                    #only living agent gets winning rewards
-                    if done:
-                        winner = np.where(np.array(rwd) == 1)[0] # TODO even dead agents get reward?
-                        if agent_inds[0] in winner:
-                            agt_rwd = 0.5
-                            logging.info(f"Win rewarded with {agt_rwd} for each living agent")
-                    #draw reward for living agents
-                    if steps_n == max_steps:
-                        done = True
-                        if agt.is_alive:
-                            agt_rwd = 0.0
-                            logging.info(f"Draw rewarded with {agt_rwd} for each living agent")
-                    #death reward
-                    if alive[i] and agent_ids[i] not in nobs[agent_inds[i]]['alive']:
-                        agt_rwd = -0.5
-                        logging.info(f"Death of agent {agent_inds[i]} rewarded with {agt_rwd}")
-                    if alive[i]:
-                        # Build original transition
-                        if p.use_memory:
-                            agt_obs = agt.get_memory_view()
-                            agt.update_memory(nobs[agent_inds[i]])
-                            agt_nobs = agt.get_memory_view()
-                            act_no_msg = act[agent_inds[i]][0] if p.communicate else act[agent_inds[i]]
-                            transition = (agt_obs, act_no_msg, agt_rwd * 100, \
-                                          agt_nobs, done)
-                        else:
-                            transition = (transformer(obs[agent_inds[i]]), act[agent_inds[i]], agt_rwd*100, \
-                                          transformer(nobs[agent_inds[i]]), done)
-                        transitions = [transition]
-                        # Create new transitions
-                        for augmentor in self.augmentors:
-                            transition_augmented = augmentor.augment(obs[agent_inds[i]], act[agent_inds[i]], agt_rwd*100, nobs[agent_inds[i]], not alive)
-                            for t in transition_augmented:
-                                transitions.append((transformer(t[0]), t[1], t[2]*100, transformer(t[3]), t[4]))
-
-                        # Add everything to the buffer
-                        for t in transitions:
-                            self.replay_buffers[i].add_to_buffer(*t)
-
-                    if alive[i]:
-                        ep_rwd += agt_rwd
-                    alive[i] = agent_ids[i] in nobs[agent_inds[i]]['alive']
-                obs = nobs
-                steps_n += 1
-            avg_rwd += ep_rwd
-            avg_steps += steps_n
-            winner = np.where(np.array(rwd) == 1)[0]
-            if len(winner) == 0:
-                ties += 1
-            else:
-                k = True
-                for i in range(self.player_agents_n):
-                    if agent_inds[i] in winner:
-                        res[0] += 1
-                        k = False
-                        break
-                if k:
-                    res[1] += 1
-
-            env.close()
-        avg_rwd /= episodes
-        avg_steps /= episodes
-        logging.info(f"Wins: {res}, Ties: {ties}, Avg. Reward: {avg_rwd}, Avg. Steps: {avg_steps}")
+        average_reward = total_reward/episodes
+        average_steps = total_steps/episodes
+        logging.info(f"Wins: {res}, Ties: {ties}, Avg. Reward: {average_reward}, Avg. Steps: {average_steps}")
         if p.reward_func == "SkynetReward":
             logging.info(
                 f"Skynet Reward split agent 1: Kills: {skynet_reward_log[0][0]}, Items: {skynet_reward_log[0][1]},"
@@ -222,6 +127,109 @@ class DataGeneratorPommerman:
                 f"Skynet Reward split agent 2: Kills: {skynet_reward_log[1][0]}, Items: {skynet_reward_log[1][1]},"
                         f" Steps(FIFO): {skynet_reward_log[1][2]}, Bombs: {skynet_reward_log[1][3]}, Deaths: {skynet_reward_log[1][4]}")
         logging.info(act_counts)
-        self.logger.write(res, ties, avg_rwd)
+        self.logger.write(res, ties, average_reward)
         # TODO: Change the return type to something more readable outside the function
-        return (res, ties, avg_rwd, act_counts, avg_steps)
+        return (res, ties, average_reward, act_counts, average_steps)
+
+    def generate_episode(self, agent1, agent2, policy1, policy2, enemy, transformer, max_steps):
+        res = np.array([0.0] * 2)
+        act_counts = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+        ties = 0.0
+        reward = 0.0
+        steps = 0.0
+
+        fifo = [[] for _ in range(self.agents_n)]
+        skynet_reward_log = [[0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0]]
+        agent_inds, agent_ids, agent_list = self._init_agent_list(agent1, agent2, policy1, policy2
+                                                                    , enemy, transformer, p.set_position)
+        env = pommerman.make(self.env, agent_list)
+        obs = env.reset()
+        done = False
+        ep_rwd = 0.0
+        alive = [True, True]
+        steps_n = 0
+        for i in range(self.agents_n):
+            fifo[i].clear()
+
+        while not done:
+            act = env.act(obs)
+            first_act = [i[0] if type(i) == list else i for i in act]
+            for j in range(self.player_agents_n):
+                act_counts[j][int(first_act[agent_inds[j]])] += 1
+            nobs, rwd, done, _ = env.step(act)
+            if p.reward_func == "SkynetReward":
+                skynet_rwds = skynet_reward(obs, act, nobs, fifo, agent_inds, skynet_reward_log)
+            for i in range(self.player_agents_n):
+                agt: TrainAgent = agent_list[agent_inds[i]]
+                if p.reward_func == "SkynetReward":
+                    agt_rwd = skynet_rwds[agent_inds[i]]
+                elif p.reward_func == "BombReward":
+                    agt_rwd = bomb_reward(nobs, act, agent_inds[i])/100
+                else:
+                    agt_rwd = staying_alive_reward(nobs, agent_ids[i])
+                    # woods close to bomb reward
+                # if act[agent_inds[i]] == Action.Bomb.value:
+                #     agent_obs = obs[agent_inds[i]]
+                #     agt_rwd += woods_close_to_bomb_reward(agent_obs, agent_obs['position'], agent_obs['blast_strength'], agent_ids)
+                #only living agent gets winning rewards
+                if done:
+                    winner = np.where(np.array(rwd) == 1)[0] # TODO even dead agents get reward?
+                    if agent_inds[0] in winner:
+                        agt_rwd = 0.5
+                        logging.info(f"Win rewarded with {agt_rwd} for each living agent")
+                #draw reward for living agents
+                if steps_n == max_steps:
+                    done = True
+                    if agt.is_alive:
+                        agt_rwd = 0.0
+                        logging.info(f"Draw rewarded with {agt_rwd} for each living agent")
+                #death reward
+                if alive[i] and agent_ids[i] not in nobs[agent_inds[i]]['alive']:
+                    agt_rwd = -0.5
+                    logging.info(f"Death of agent {agent_inds[i]} rewarded with {agt_rwd}")
+                if alive[i]:
+                    # Build original transition
+                    if p.use_memory:
+                        agt_obs = agt.get_memory_view()
+                        agt.update_memory(nobs[agent_inds[i]])
+                        agt_nobs = agt.get_memory_view()
+                        act_no_msg = act[agent_inds[i]][0] if p.communicate else act[agent_inds[i]]
+                        transition = (agt_obs, act_no_msg, agt_rwd * 100, \
+                                        agt_nobs, done)
+                    else:
+                        transition = (transformer(obs[agent_inds[i]]), act[agent_inds[i]], agt_rwd*100, \
+                                        transformer(nobs[agent_inds[i]]), done)
+                    transitions = [transition]
+                    # Create new transitions
+                    for augmentor in self.augmentors:
+                        transition_augmented = augmentor.augment(obs[agent_inds[i]], act[agent_inds[i]], agt_rwd*100, nobs[agent_inds[i]], not alive)
+                        for t in transition_augmented:
+                            transitions.append((transformer(t[0]), t[1], t[2]*100, transformer(t[3]), t[4]))
+
+                    # Add everything to the buffer
+                    for t in transitions:
+                        self.replay_buffers[i].add_to_buffer(*t)
+
+                if alive[i]:
+                    ep_rwd += agt_rwd
+                alive[i] = agent_ids[i] in nobs[agent_inds[i]]['alive']
+            obs = nobs
+            steps_n += 1
+        reward += ep_rwd
+        steps += steps_n
+        winner = np.where(np.array(rwd) == 1)[0]
+        if len(winner) == 0:
+            ties += 1
+        else:
+            k = True
+            for i in range(self.player_agents_n):
+                if agent_inds[i] in winner:
+                    res[0] += 1
+                    k = False
+                    break
+            if k:
+                res[1] += 1
+
+        env.close()
+        return (reward, steps, res, ties, act_counts, skynet_reward_log)
