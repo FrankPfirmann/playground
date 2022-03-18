1e88545a6b89d9f2c72e31f8bcb3851f4fccb469
tgru <21686590+tgru@users.noreply.github.com>
2022-03-18 13:55:42 +0100

    Wrap model setup in DQN agent and update test_model_separate with it

---
+++ pommermanLearn/agents/dqn_agent.py
@@ -0,0 +1,41 @@
+import torch
+from pommerman.agents import BaseAgent
+from agents.train_agent import TrainAgent
+from models import Pommer_Q
+from dqn import DQN
+from util.data import transform_observation_partial, transform_observation_partial_uncropped
+import params as p
+
+class DQNAgent(BaseAgent):
+    """
+    A DQN based agent for the Pommerman environment
+    """
+    def __init__(self, model_dir):
+        super(DQNAgent, self).__init__()
+        support = torch.linspace(p.v_min, p.v_max, p.atom_size).to(p.device)
+        transformer = transform_observation_partial_uncropped
+        q = Pommer_Q(transform_observation_partial, support)
+        q.load_state_dict(torch.load(model_dir, map_location=p.device))
+        q.to(torch.device(p.device))
+        q.eval()
+        q_target = Pommer_Q(transform_observation_partial, support)
+        q_target.load_state_dict(torch.load(model_dir, map_location=p.device))
+        q_target.to(torch.device(p.device))
+        q_target.eval()
+        dqn = DQN(q, q_target, p.exploration_noise, device=torch.device(p.device), dq=p.double_q, support=support)
+
+        transformer = q.get_transformer()
+        self._agent=TrainAgent(policy=dqn.get_policy(), transformer=transformer, is_train=False)
+
+    def init_agent(self, id, game_type):
+        super(DQNAgent, self).init_agent(id, game_type)
+        return self._agent.init_agent(id, game_type)
+
+    def act(self, observation, action_space):
+        return self._agent.act(observation, action_space)
+
+    def episode_end(self, reward):
+        return self._agent.episode_end(reward)
+
+    def shutdown(self):
+        return self._agent.shutdown()
--- pommermanLearn/test_model_separate.py
+++ pommermanLearn/test_model_separate.py
@@ -1,100 +1,83 @@
-# solve error #15
-import os
-
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
-from datetime import datetime
-import os
-import torch
 import numpy as np
-import params as p
-
-from models import Pommer_Q
-from dqn import DQN
-from util.data import transform_observation, transform_observation_centralized, transform_observation_partial
-import params as p
 
 import pommerman
+from agents.dqn_agent import DQNAgent
 from pommerman.agents import SimpleAgent
-from agents.train_agent import TrainAgent
 from agents.skynet_agents import SmartRandomAgent, SmartRandomAgentNoBomb, StaticAgent
+from pommerman.agents.http_agent import HttpAgent
 
-episodes = 50
-
-model_dir1 = './data/tensorboard/20220127T193625/549_1'
-model_dir2 = './data/tensorboard/20220127T193625/549_2'
-
-
+episodes = 100
 render = False
-# render = True
-enemy = SmartRandomAgent
 
-# get policy of models
-q1 = Pommer_Q(p.p_observable, transform_observation_partial)
-q_target1 = Pommer_Q(p.p_observable, transform_observation_partial)
-q1.load_state_dict(torch.load(model_dir1))
-q_target1.load_state_dict(torch.load(model_dir1))
-algo1 = DQN(q1, q_target1, p.exploration_noise)
-algo1.set_train(False)
-policy1 = algo1.get_policy()
+def get_agents():
+    # get policy of models
+    model_dir1 = "./data/tensorboard/communicate_2/99_1" # Agent 1
+    model_dir2 = "./data/tensorboard/communicate_2/99_2" # Agent 2
 
-q2 = Pommer_Q(p.p_observable, transform_observation_partial)
-q_target2 = Pommer_Q(p.p_observable, transform_observation_partial)
-q2.load_state_dict(torch.load(model_dir2))
-q_target2.load_state_dict(torch.load(model_dir2))
-algo2 = DQN(q2, q_target2, p.exploration_noise)
-algo2.set_train(False)
-policy2 = algo2.get_policy()
+    agent1 = DQNAgent(model_dir1)
+    agent2 = DQNAgent(model_dir2)
 
-# setup env
-if np.random.randint(2) == 0:
-    agent_list = [
-        TrainAgent(policy1),
-        enemy(),
-        TrainAgent(policy2),
-        enemy(),
-    ]
-else:
-    agent_list = [
-        enemy(),
-        TrainAgent(policy1),
-        enemy(),
-        TrainAgent(policy2),
-    ]
+    # Prepare enemy agents
+    enemy1  = StaticAgent()
+    enemy2  = StaticAgent()
+    #enemy1  = SimpleAgent()
+    #enemy2  = SimpleAgent()
+    #enemy1  = SmartRandomAgent()
+    #enemy2  = SmartRandomAgent()
+    #enemy1  = SmartRandomAgentNoBomb()
+    #enemy2  = SmartRandomAgentNoBomb()
+    #enemy1 = HttpAgent(port=10080, host='localhost'),
+    #enemy2 = HttpAgent(port=10081, host='localhost'),
 
-env = pommerman.make('PommeTeamCompetition-v0', agent_list)
+    if np.random.randint(2) == 0:
+        return (0,[
+            agent1,
+            enemy1,
+            agent2,
+            enemy2,
+        ])
+    else:
+        return (1, [
+            enemy1,
+            agent1,
+            enemy2,
+            agent2
+        ])
 
 wins = 0
 ties = 0
 losses = 0
+steps_wl = []
+steps_t  = []
 
+print(f"Starting test run with {episodes} games")
 for i in range(episodes):
-    print(i)
     episode_reward = 0
     # run game
+    idx, agent_list = get_agents()
+    env = pommerman.make('PommeRadio-v2', agent_list)
     obs = env.reset()
     done = False
+    step = 0
     while not done:
         if render == True:
             env.render()
         actions = env.act(obs)
         obs, reward, done, info = env.step(actions)
         episode_reward += reward[0]
+        step+=1
     env.render(close=True)
     env.close()
     # count wins, ties, losses
     if 'winners' in info:
-        if info['winners'][0] == 0:
+        if info['winners'][0] == idx:
             wins += 1
         else:
             losses += 1
+        steps_wl.append(step)
     else:
         ties += 1
+        steps_t.append(step)
 
-    print(wins/(i+1))
-    print(ties/(i+1))
-    print(losses/(i+1))
-
-print(wins/episodes)
-print(ties/episodes)
-print(losses/episodes)
+    print(f"Episode {i+1}; Wins: {wins}/{100*wins/(i+1)}%; Ties: {ties}/{100*ties/(i+1)}%; Losses: {losses}/{100*losses/(i+1)}%; Steps(WL): {sum(steps_wl)/len(steps_wl) if len(steps_wl)>0 else 0}; Steps(T): {sum(steps_t)/len(steps_t) if len(steps_t)>0 else 0}")
+print("Finished test!")
